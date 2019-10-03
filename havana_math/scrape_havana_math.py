1053412f2953f02d3a4293bdce02d40caef6e1f1
Matthew Brett <matthew.brett@gmail.com>
2019-10-03 11:54:56 +0100

    Notes on processing.

--- havana_math/scrape_havana_math.py
+++ havana_math/scrape_havana_math.py
@@ -1,3 +1,5 @@
+""" Scrape HTML output from PDF with mathematics marks
+"""
 import os.path as op
 
 import numpy as np
@@ -6,16 +8,31 @@ import pandas as pd
 from bs4 import BeautifulSoup
 
 
+# Read the HTML into memory.
 with open('matematica_notas_ordinaria.html', 'rt') as fobj:
     html_doc = fobj.read()
+
+# Parse HTML into objects.
 soup = BeautifulSoup(html_doc, 'html.parser')
 
+# All marks and relevant text are in <div> tags.
 pages = [c for c in soup.body.children if c.name == 'div']
-# From looking at the PDF
+
+# Check found number of pages against last page number in the PDF.
 assert len(pages) == 307
 
 
 def process_page(page):
+    """ Parse page data into list of student entries.
+
+    One student entry is a list with values corresponding to:
+
+    * school
+    * school_type
+    * id
+    * name
+    * mark (float)
+    """
     divs = [c for c in page.children if c.name == 'div']
     entries = [e.text for e in divs if 'cls_007' in e.attrs.get('class', [])]
     school, via = entries[0:2]
@@ -25,14 +42,21 @@ def process_page(page):
     students = []
     for i in range(0, len(s_entries), 3):
         id_no, name, mark = s_entries[i:i+3]
+        # Marks have , for decimal separator.
+        # Aus means "Ausente" - absent.  Replace with NaN.
         mark = np.nan if mark == 'Aus' else float(mark.replace(',', '.'))
         students.append([school, via, id_no, name, mark])
     return students
 
 
+# Concatenate the list from each page into one list.
 students = sum([process_page(p) for p in pages], [])
+
+# Convert to data frame.
 df = pd.DataFrame(students, columns=['school', 'school_type', 'id', 'name', 'mark'])
 
+# Do some checks.
+#
 # From: https://proyectoinventario.org/resultados-examen-ingreso-matematica-habana
 #
 # De los 7 735 estudiantes que debían tomar el examen, no se presentaron 775,
@@ -40,7 +64,7 @@ df = pd.DataFrame(students, columns=['school', 'school_type', 'id', 'name', 'mar
 assert len(df) == 7735
 assert np.sum(df['mark'].isna()) == 775
 
-# Later - this list of Vía de ingreso:
+# Later on the same page - this list of Vía de ingreso:
 VIAS = ('Academias Deportivas de Alto Rendimiento',
         'Cadetes MININT',
         'Concurso',
