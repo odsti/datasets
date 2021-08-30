dc267b0cdbe466150610180ef4e7e1d7cbece48c
Matthew Brett <matthew.brett@gmail.com>
2021-08-30 14:57:39 +0100

    Rename Diet column for consistency

--- sheffield_diet/process_diet.py
+++ sheffield_diet/process_diet.py
@@ -10,10 +10,11 @@ orig_fname = op.join('original', 'Diet_SPSS.sav')
 df = pd.read_spss(orig_fname)
 
 simple_df = df.loc[:, ['Diet', 'weightLOST']].rename(columns={
+    'Diet': 'diet',
     'weightLOST': 'weight_lost'})
 
 # Recode diet column to strings to make clear it is categorical.
-simple_df['Diet'] = simple_df['Diet'].replace(
+simple_df['diet'] = simple_df['diet'].replace(
     {1: 'A', 2: 'B', 3: 'C'})
 
 proc_fname = op.join('processed', 'sheffield_diet.csv')
